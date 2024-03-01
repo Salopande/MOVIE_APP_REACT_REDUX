8c5481f4d59bd0521f5fabd5bb62717d68e7d6dd
Saloni <pandesaloni2@gmail.com>
2024-03-01 16:14:31 +0530

    fixed small bug

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -5,49 +5,50 @@ import MovieCard from "../MovieCard/MovieCard";
 import "./MovieListing.scss";
 import Slider from "react-slick";
 import { Settings } from "../../utils/settings";
+
 const MovieListing = () => {
-  
   const movieData = useSelector(getAllMovie);
   const showsData = useSelector(getAllShows);
-  console.log(showsData);
-
+  
   let renderMovies = "";
   let renderShows = "";
 
   renderMovies =
-    movieData.Response === "True" ? (
-      movieData.Search.map((movie, index) => (
-        <MovieCard key={index} data={movie} />
-      ))
-    ) : (
-      <div className="movies-error">
-        <h3>{movieData.Error}</h3>
-      </div>
-    );
+    movieData.Response === "True" &&
+    movieData.Search.map((movie, index) => (
+      <MovieCard key={index} data={movie} />
+    ));
 
   renderShows =
-    showsData.Response === "True" ? (
-      showsData.Search.map((movie, index) => (
-        <MovieCard key={index} data={movie} />
-      ))
-    ) : (
-      <div className="movies-error">
-        <h3>{showsData.Error}</h3>
-      </div>
-    );
+    showsData.Response === "True" &&
+    showsData.Search.map((movie, index) => (
+      <MovieCard key={index} data={movie} />
+    ));
 
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">
-          <Slider {...Settings}>{renderMovies} </Slider>
+          {movieData.Response === "True" ? (
+            <Slider {...Settings}>{renderMovies} </Slider>
+          ) : (
+            <div className="movies-error">
+              <h3>{movieData.Error}</h3>
+            </div>
+          )}
         </div>
       </div>
       <div className="shows-list">
         <h2>Shows</h2>
         <div className="movie-container">
-          <Slider {...Settings}>{renderShows}</Slider>
+          {showsData.Response === "True" ? (
+            <Slider {...Settings}>{renderShows}</Slider>
+          ) : (
+            <div className="movies-error">
+              <h3>{showsData.Error}</h3>
+            </div>
+          )}
         </div>
       </div>
     </div>
